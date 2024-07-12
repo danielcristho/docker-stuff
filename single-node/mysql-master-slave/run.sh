4dacf9c66f091cfdb2f538e483005dfa5f024c06
danielcristho <pepuhodaniel93.learn@gmail.com>
2024-07-12 20:11:23 +0700

    fix: replication script

--- single-node/mysql-master-slave/run.sh
+++ single-node/mysql-master-slave/run.sh
@@ -21,6 +21,10 @@ echo "Connected to Master: $MASTER_HOST"
 priv_stmt="GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_USER'@'%'; FLUSH PRIVILEGES;"
 docker exec mysql_master sh -c "mysql -u root -e \"$priv_stmt\""
 
+MS_STATUS=$(docker exec mysql_master sh -c 'mysql -u root -e "SHOW MASTER STATUS\G"')
+CURRENT_LOG=$(echo "$MS_STATUS" | grep "File:" | awk '{print $2}')
+CURRENT_POS=$(echo "$MS_STATUS" | grep "Position:" | awk '{print $2}')
+
 while ! mysqladmin ping -h $SLAVE_HOST --silent; do
 	sleep 1
 done
@@ -31,10 +35,6 @@ priv_stmt="GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_USER'@'%'; FLUSH PRIVILEGES
 docker exec mysql_slave sh -c "mysql -u root -e \"$priv_stmt\""
 
 docker exec mysql_slave sh -c "mysql -u root -e 'STOP SLAVE;'"
-
-MS_STATUS=$(docker exec mysql_master sh -c 'mysql -u root -e "SHOW MASTER STATUS\G"')
-CURRENT_LOG=$(echo "$MS_STATUS" | grep "File:" | awk '{print $2}')
-CURRENT_POS=$(echo "$MS_STATUS" | grep "Position:" | awk '{print $2}')
 
 start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$MASTER_HOST',MASTER_USER='$MYSQL_USER',MASTER_PASSWORD='$MYSQL_PASSWORD',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
 echo "Executing: $start_slave_stmt"
