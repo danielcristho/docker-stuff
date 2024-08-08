3f498a6008adfefb2c41256b93000d8a3a7d4bd3
danielcristho <pepuhodaniel93.learn@gmail.com>
2024-08-08 20:20:10 +0700

    fix: load env from .env file

--- single-node/mysql-master-slave/run.sh
+++ single-node/mysql-master-slave/run.sh
@@ -2,7 +2,12 @@
 
 #!/bin/bash
 
-source .env
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+else
+    echo ".env file not found!"
+    exit 1
+fi
 
 docker-compose down -v
 rm -rf ./master/data/*
@@ -24,6 +29,7 @@ docker exec mysql_master sh -c "mysql -u root -e \"$priv_stmt\""
 MS_STATUS=$(docker exec mysql_master sh -c 'mysql -u root -e "SHOW MASTER STATUS\G"')
 CURRENT_LOG=$(echo "$MS_STATUS" | grep "File:" | awk '{print $2}')
 CURRENT_POS=$(echo "$MS_STATUS" | grep "Position:" | awk '{print $2}')
+echo "Master status: $MS_STATUS"
 
 while ! mysqladmin ping -h $SLAVE_HOST --silent; do
 	sleep 1
@@ -36,7 +42,12 @@ docker exec mysql_slave sh -c "mysql -u root -e \"$priv_stmt\""
 
 docker exec mysql_slave sh -c "mysql -u root -e 'STOP SLAVE;'"
 
-start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$MASTER_HOST',MASTER_USER='$MYSQL_USER',MASTER_PASSWORD='$MYSQL_PASSWORD',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
+start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$MASTER_HOST',
+	MASTER_USER='$MYSQL_USER',
+	MASTER_PASSWORD='$MYSQL_PASSWORD',
+	MASTER_LOG_FILE='$CURRENT_LOG',
+	MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
+
 echo "Executing: $start_slave_stmt"
 docker exec mysql_slave sh -c "mysql -u root --execute=\"$start_slave_stmt\""
 
